1062c2110f851fde924c589a599b46047c538e77
Edwin <edwin.h.morris@gmail.com>
2013-01-26 01:11:49 -0500

    open tab after facebook tab is closed

--- background.js
+++ background.js
@@ -3,3 +3,19 @@ var x = []
 chrome.tabs.onCreated.addListener(function(tab) {
   console.dir(tab)
 });
+
+function tab_was_facebook() {
+  return true;
+}
+
+function past_time_threshold() {
+  return true;
+}
+
+chrome.tabs.onRemoved.addListener(function(tab_id) {
+  if (tab_was_facebook() && past_time_threshold()) {
+    chrome.tabs.create({
+      'url': 'http://fbhappiness-app.herokuapp.com/'
+    });
+  }
+});
