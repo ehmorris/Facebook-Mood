11967d36add7e3c99ccc9b4a446a75c112bb09f9
Nathan Lilienthal <nathanlil13@me.com>
2013-01-26 02:44:32 -0500

    TAB WORK

--- background.js
+++ background.js
@@ -1,9 +1,32 @@
-var x = []
+// Keep an updated record of the users tabs in Pages.
+var Pages = [];
 
 chrome.tabs.onCreated.addListener(function(tab) {
-  console.dir(tab)
+  Pages.push({
+    id: tab.id,
+    url: tab.url,
+    duration: 0
+  });
 });
 
+chrome.tabs.onUpdated.addListener(function(id) {
+  for (var i = Pages.length - 1; i >= 0; i--) {
+    if (Pages[i].id == id) {
+      var page = Pages[i];
+      chrome.tabs.get(id, function(tab) {
+        page.url = tab.url;
+      });
+    }
+  }
+});
+
+chrome.tabs.onRemoved.addListener(function(id) {
+  for (var i = Pages.length - 1; i >= 0; i--) {
+    if (Pages[i].id == id) {
+      Pages.splice(i, 1);
+    }
+  }
+});
 function tab_was_facebook() {
   return true;
 }
