25ed689eef2c8d6988971bd0aa27b7825b73cce1
Nathan Lilienthal <nathanlil13@me.com>
2013-01-26 03:01:11 -0500

    open new tab when closing facebook

--- background.js
+++ background.js
@@ -1,5 +1,17 @@
 // Keep an updated record of the users tabs in Pages.
+// Pages is an array of page's, where a page is:
+// {id: int, url: string, duration: int}
+//
 var Pages = [];
+
+function tab_was_facebook(page) {
+  console.log(page);
+  return !!page.url.match(/facebook.com/);
+}
+
+function past_time_threshold(page) {
+  return true;
+}
 
 chrome.tabs.onCreated.addListener(function(tab) {
   Pages.push({
@@ -21,24 +33,16 @@ chrome.tabs.onUpdated.addListener(function(id) {
 });
 
 chrome.tabs.onRemoved.addListener(function(id) {
+  var removed;
   for (var i = Pages.length - 1; i >= 0; i--) {
     if (Pages[i].id == id) {
+      removed = Pages[i];
       Pages.splice(i, 1);
     }
   }
-});
-function tab_was_facebook() {
-  return true;
-}
 
-function past_time_threshold() {
-  return true;
-}
-
-chrome.tabs.onRemoved.addListener(function(tab_id) {
-  if (tab_was_facebook() && past_time_threshold()) {
-    chrome.tabs.create({
-      'url': 'http://fbhappiness-app.herokuapp.com/'
-    });
+  // If facebook gets closed we do stuff.
+  if (tab_was_facebook(removed) && past_time_threshold(removed)) {
+    chrome.tabs.create({'url': 'http://fbhappiness-app.herokuapp.com/'});
   }
 });
