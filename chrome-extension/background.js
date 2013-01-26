24e57c12ad17d7204a665549c6b36e32b47f98ac
Edwin <edwin.h.morris@gmail.com>
2013-01-26 16:06:58 -0500

    extension opens app on install

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -53,3 +53,33 @@ chrome.tabs.onRemoved.addListener(function(id) {
     });
   }
 });
+
+// open help page if extension is new or updated
+
+function onInstall() {
+  chrome.windows.create({
+    'url': 'http://fbmood.herokuapp.com'
+  });
+}
+
+function onUpdate() {
+  return false;
+}
+
+function getVersion() {
+  var details = chrome.app.getDetails();
+  return details.version;
+}
+
+// Check if the version has changed.
+var currVersion = getVersion();
+var prevVersion = localStorage['version']
+if (currVersion != prevVersion) {
+  // Check if we just installed this extension.
+  if (typeof prevVersion == 'undefined') {
+    onInstall();
+  } else {
+    onUpdate();
+  }
+  localStorage['version'] = currVersion;
+}
