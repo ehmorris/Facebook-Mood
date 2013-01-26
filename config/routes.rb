a2079282689e0c908eee1ce78a3d191ad7d99325
Edwin <edwin.h.morris@gmail.com>
2013-01-26 04:47:33 -0500

    redirect unloggedin users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ FbhappinessApp::Application.routes.draw do
 
   constraints Clearance::Constraints::SignedOut.new do
     root to: 'application#index'
+    match '/moods/new' => 'application#index'
+    match '/moods/index' => 'application#index'
   end
 
   constraints Clearance::Constraints::SignedIn.new do
